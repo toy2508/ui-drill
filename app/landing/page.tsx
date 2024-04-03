6bbbee342d15e3d5cf0c194ef78f7cca44108c74
toy2508 <toy2508@naver.com>
2024-04-03 12:09:43 +0900

    Initial commit from Create Next App

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -7,7 +7,8 @@ export default function LadingPage() {
 
     return (
         <div className={`${poppins.className} bg-neutral-900 w-full h-screen flex place-items-center justify-center`}>
-            <section>
+            <section className={"w-[44rem] h-[26rem] bg-neutral-800 rounded-lg border-neutral-600 " +
+                "flex flex-row p-8 absolute justify-between stroke-[0.1] hover:stroke-[0.2]"}>
                 <div></div>
             </section>
         </div>
